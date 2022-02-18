e6db47e5ad1907fbcd19270d5ef1acc0f514b9a2
Edin Sikira <edinsikira5@gmail.com>
2022-02-18 15:19:38 +0100

    Changes to background.

--- src/components/BackgroundImage.jsx
+++ src/components/BackgroundImage.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from "react";
 import { useGlobalContext } from "../context";
 
 function BackgroundImage({ layout }) {
@@ -8,8 +9,21 @@ function BackgroundImage({ layout }) {
     ? `${require(`../images/weather-backgrounds/${IS_DAY_NIGHT}/${backgroundImage}.jpg`)}`
     : "";
 
+  const bcgRef = useRef(null);
+
+  // FIX | Mobile background image cropped on scroll because of the URL bars
+  useEffect(() => {
+    const resizeBackground = (e) => {
+      const adjustedHeight = e.target.innerHeight + 5;
+      bcgRef.current.style.height = `${adjustedHeight}px`;
+    };
+
+    window.addEventListener("resize", resizeBackground);
+    return () => window.removeEventListener("resize", resizeBackground);
+  });
+
   return (
-    <div className="background-image">
+    <div className="background-image" ref={bcgRef}>
       <img
         src={img}
         alt=""
